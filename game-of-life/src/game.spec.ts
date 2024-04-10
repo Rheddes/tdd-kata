f3f70e77db50bdd211482ae717575d8e13a77bcc
Rens Heddes <rens.h@me.com>
2024-04-10 08:29:48 +0200

    red

--- game-of-life/src/game.spec.ts
+++ game-of-life/src/game.spec.ts
@@ -5,4 +5,10 @@ describe('Game of Life', () => {
     it('works for an empty board', () => {
         expect(next_generation([[]])).to.deep.equal([[]]);
     });
+
+    describe('dead cell', () => {
+        it('stays dead with no neighbours', () => {
+            expect(next_generation([[false]])).to.deep.equal([[false]]);
+        });
+    });
 });
