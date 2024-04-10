0887f2f6bd2c71a8a8ee76ef84ccc251b75a503b
Rens Heddes <rens.h@me.com>
2024-04-10 08:56:56 +0200

    refactor

--- game-of-life/src/game.ts
+++ game-of-life/src/game.ts
@@ -1,7 +1,13 @@
 export function next_generation(board: boolean[][]): boolean[][] {
     return board.map((row, row_idx) => row.map((cell, col_idx) => {
-        if (board[row_idx-1]?.[col_idx] && board[row_idx+1]?.[col_idx] && row[col_idx-1]) return true;
-        if (row[col_idx-1] && row[col_idx+1]) return cell;
+        const nofAliveNeighbours = [
+            board[row_idx-1]?.[col_idx],
+            row[col_idx-1],
+            row[col_idx+1],
+            board[row_idx+1]?.[col_idx],
+        ].filter(c => c).length;
+        if (nofAliveNeighbours === 3) return true;
+        if (nofAliveNeighbours === 2) return cell;
         return false;
     }));
 }
