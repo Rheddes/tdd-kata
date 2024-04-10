39296a06b84bd00f8625cb93b99dd4a66d716b24
Rens Heddes <rens.h@me.com>
2024-04-10 13:18:11 +0200

    green

--- game-of-life/src/game.ts
+++ game-of-life/src/game.ts
@@ -1,6 +1,6 @@
 export function next_generation(board: boolean[][]): boolean[][] {
     return board.map(row => row.map((cell, col_idx) => {
-        if (row[col_idx-1] && row[col_idx+1]) return true;
+        if (row[col_idx-1] && row[col_idx+1]) return cell;
         return false;
     }));
 }
