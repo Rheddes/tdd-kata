38b067d578898b5203446552b6d9783218ddf569
Rens Heddes <rens.h@me.com>
2024-04-10 08:31:07 +0200

    refactor

--- game-of-life/src/game.spec.ts
+++ game-of-life/src/game.spec.ts
@@ -1,6 +1,8 @@
 import { describe, it, expect } from 'vitest';
 import { next_generation } from './game';
 
+const _: boolean = false;
+
 describe('Game of Life', () => {
     it('works for an empty board', () => {
         expect(next_generation([[]])).to.deep.equal([[]]);
@@ -8,7 +10,7 @@ describe('Game of Life', () => {
 
     describe('dead cell', () => {
         it('stays dead with no neighbours', () => {
-            expect(next_generation([[false]])).to.deep.equal([[false]]);
+            expect(next_generation([[_]])).to.deep.equal([[_]]);
         });
     });
 });
